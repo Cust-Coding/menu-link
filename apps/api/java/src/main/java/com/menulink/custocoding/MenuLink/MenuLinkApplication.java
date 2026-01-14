ad1f701b272c8d12d494c3ea7b7cc1b83b7169d0
WesleyBule <bulewesley7@gmail.com>
2026-01-14 14:16:24 +0200

    chore: initialize Spring Boot project

---
+++ apps/api/java/src/main/java/com/menulink/custocoding/MenuLink/MenuLinkApplication.java
@@ -0,0 +1,13 @@
+package com.menulink.custocoding.MenuLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuLinkApplication.class, args);
+	}
+
+}
---
+++ apps/api/java/src/test/java/com/menulink/custocoding/MenuLink/MenuLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menulink.custocoding.MenuLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
