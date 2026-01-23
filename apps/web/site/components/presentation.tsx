fc6bc2af8ab8916aba29383e6e8513b548ded9d4
CUSTcoding <custodiotitossetitosse@gmail.com>
2026-01-22 18:02:28 -0600

    feat(ui) Create presentation component

---
+++ apps/web/site/components/presentation.tsx
@@ -0,0 +1,9 @@
+export default function Presentation(){
+    return(
+        <div className="">
+            <div className="text">
+                <h2></h2>
+            </div>
+        </div>
+    );
+}
